8b67800d57a44e59441526c4c1e3746a146b8821
Divyanshi <153212843+divyanshivermaa@users.noreply.github.com>
2025-10-03 00:03:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 
 const API_CONFIG = {
-    OPENWEATHER_API_KEY: '1ec0f86dc65448f7021c9fe1f7079d27',
+    OPENWEATHER_API_KEY: 'your_api_key',
     BASE_URL: 'https://api.openweathermap.org/data/2.5'
 };
+
 
 export default API_CONFIG; 
